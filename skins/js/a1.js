6fafd94a83821102c0988c42400d3c1dd4143950
lijingdan2016 <973327127@qq.com>
2018-09-20 17:32:40 +0800

    提交

---
+++ skins/js/a1.js
@@ -0,0 +1,29 @@
+ function change1(){
+	var d = document.getElementById("d1");
+    d.style.backgroundColor = "#a9a9a9";
+}
+ 
+ function change2(){
+	var d = document.getElementById("d1");
+    d.style.backgroundColor = "#9370db";
+}
+ 
+ function change3(){
+	var d = document.getElementById("d1");
+    d.style.backgroundColor = "#ff0000";
+}
+ 
+ function change4(){
+	var d = document.getElementById("d1");
+    d.style.backgroundColor = "#87cfeb";
+}
+ 
+ function change5(){
+	var d = document.getElementById("d1");
+    d.style.backgroundColor = "#ff8d00";
+}
+ 
+ function change6(){
+	var d = document.getElementById("d1");
+    d.style.backgroundColor = "#7cfd00";
+}
